0eea7cd722fd6c5cf29a051690ac4e1f565c576c
Helen Song <hakyungsong97@gmail.com>
2019-02-06 21:16:51 -0500

    final version - passed autolab

--- Hw 2/inspect.py
+++ Hw 2/inspect.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import sys
 import math
 
@@ -23,6 +24,13 @@ for j in range(totalObs) :
 pTrue = trueCount / (totalObs-1)
 pFalse = (totalObs-trueCount-1) / (totalObs-1)
 entropy = -1 * ((pTrue * math.log(pTrue,2))+(pFalse * math.log(pFalse,2)))
+
+'''
+if (pTrue == 0 and pFalse == 0): entropy = 0
+elif (pTrue == 0): entropy = -1 * (pFalse * math.log(pFalse,2))
+elif (pFalse ==0): entropy = -1 * (pTrue * math.log(pTrue,2))
+else: entropy = -1 * ((pTrue * math.log(pTrue,2))+(pFalse * math.log(pFalse,2)))
+'''
 
 #calculate error
 if (trueCount > (totalObs/2)): error = pFalse
