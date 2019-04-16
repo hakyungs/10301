2391c87d5ef2859855630eff076ba364b6b28886
Helen Song <hakyungsong97@gmail.com>
2019-04-15 22:01:09 -0400

    trans correct ver

--- Hw 7/handout/learnhmm.py
+++ Hw 7/handout/learnhmm.py
@@ -6,17 +6,30 @@ import numpy as np
 #########################################################################
 # Formatting Data
 #########################################################################
+
+
 def formatData(d,data_type):
-    if (data_type == words):
+    # train_input
+    if (data_type == 0):
         res = []
         for line in d.readlines():
-			tokens = line.strip().split(" ")
-			token_list = []
-			for t in tokens:
-				token = t.split("_")
-				token_list.append(token)
-			res.append(token_list)
-    return
+            tokens = line.strip().split(" ")
+            token_list = []
+            for t in tokens:
+                token = t.split("_")
+                token_list.append(token)
+            res.append(token_list)
+    elif (data_type == 1):  # index_to_tag & index_to_word
+        res = {}
+        row = []
+        for line in d.readlines():
+            if (line.strip() != ""):
+                row.append(line.split()[0])
+            for i in range(len(row)):
+                res[row[i]] = i
+
+    return res
+
 
 #########################################################################
 # Main Function
@@ -29,20 +42,62 @@ if __name__ == "__main__" :
     i5 = sys.argv[5]
     i6 = sys.argv[6]
 
-    train_input = open(i1,"r")
-    index_to_word = open(i2,"r")
-    index_to_tag = open(i3,"r")
-    hmmprior = open(i4,"w")
-    hmmemit = open(i5,"w")
-    hmmtrans = open(i6,"w")
+    train_input_raw = open(i1,"r")
+    index_to_word_raw = open(i2,"r")
+    index_to_tag_raw = open(i3,"r")
+    hmmprior_out = open(i4,"w")
+    hmmemit_out = open(i5,"w")
+    hmmtrans_out = open(i6,"w")
 
-    train_data = formatData(train_input,words)
-    word_idx_data = formatData(index_to_word,idx)
-    tag_idx_data = formatData(index_to_tag,idx)
+    train_input = formatData(train_input_raw,0)
+    index_to_word = formatData(index_to_word_raw,1)
+    index_to_tag = formatData(index_to_tag_raw,1)
 
-    train_input.close()
-    index_to_word.close()
-    index_to_tag.close()
-    hmmprior.close()
-    hmmemit.close()
-    hmmtrans.close()
+    w_len = len(index_to_word)
+    t_len = len(index_to_tag)
+
+    # initialize np arrays
+    hmmtrans = np.zeros((t_len,t_len))
+    hmmemit = np.zeros((t_len,w_len))
+    hmmprior = np.zeros(t_len)
+
+    # fill the matrices
+    for sentence in train_input:
+        prev = None
+        for i, pair in enumerate(sentence):
+            word_idx,tag_idx = pair[0],pair[1]
+            word,tag = index_to_word[word_idx],index_to_tag[tag_idx]
+            if (i == 0) :
+                hmmprior[tag] += 1
+            else :
+                hmmtrans[prev][tag] += 1
+            prev = tag
+            hmmemit[tag][word] += 1
+
+    hmmprior += 1
+    hmmtrans += 1
+    hmmemit += 1
+
+    #normalize
+    for row in hmmtrans:
+        row /= np.sum(row)
+    for row in hmmemit:
+        row /= np.sum(row)
+    hmmprior /= np.sum(hmmprior)
+
+
+    # write outputs
+    for row in hmmtrans:
+        toPrint = ""
+        for entry in row:
+            toNum = "%.18e"%entry
+            toPrint += " " + toNum
+        hmmtrans_out.write(toPrint + "\n")
+
+
+    train_input_raw.close()
+    index_to_word_raw.close()
+    index_to_tag_raw.close()
+    hmmprior_out.close()
+    hmmemit_out.close()
+    hmmtrans_out.close()
